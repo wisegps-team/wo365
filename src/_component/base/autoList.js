28a19d311903ba9d24fe110c75ad5ad5b0fdee41
Jianghai <marsandheart@gmail.com>
2016-09-21 17:16:49 +0800

    自动加载下一页组件替换

--- src/_component/base/autoList.js
+++ src/_component/base/autoList.js
@@ -1,68 +1,77 @@
 /**
  * 08/004
  * 小吴
- * 带自动加载的列表组件
+ * 带自动加载的高阶组件
  */
-"use strict";
 import React, {Component} from 'react';
-import {List, ListItem} from 'material-ui/List';
 
-class AutoList extends Component {
-    constructor(props, context) {
-        super(props, context);
 
-        this._loaded=!props.loading;
-    }
-    
-    componentDidMount() {
-        if(!AutoList._lists.length)
-            document.addEventListener('scroll',AutoList.scroll);
-        if(this.props.forLoad)
-            AutoList._lists.push(this);
-    }
-    componentWillUnmount() {
-        AutoList._lists=AutoList._lists.filter((ele)=>ele!=this);
-        if(!AutoList._lists.length)
-            document.removeEventListener('scroll',AutoList.scroll);
-    }
-    componentWillReceiveProps(nextProps) {
-        this._loaded=!nextProps.loading;
-        if(this.props.forLoad!=nextProps.forLoad){
-            if(nextProps.forLoad)
-                AutoList._lists.push(this);
-            else
-                AutoList._lists=AutoList._lists.filter((ele)=>ele!=this);
-        }   
-    }
-    
-    load(){
-        if(this._loaded){
-            this._loaded=false;
-            this.props.load();
+function AutoList(ShowComponent){
+    return class Com extends Component{
+        constructor(props, context) {
+            super(props, context);
+            this.state={
+                page:0,//已经加载的页
+                waiting:false
+            }
+            this.setPage();
+            this.scroll = this.scroll.bind(this);
+        }
+        componentDidMount() {
+            document.addEventListener('scroll',this.scroll);
+        }
+        componentWillUnmount() {
+            document.removeEventListener('scroll',this.scroll);
+        }
+        componentWillReceiveProps(nextProps) {
+            let waiting = this.state.waiting&&(nextProps.data.length==this.props.data.length);
+            this.setState({waiting});
+            this.setPage(nextProps);
+        }
+
+        setPage(props){
+            if(!props){
+                this.state.page=Math.ceil(this.props.data.length/this.props.limit);
+            }else{
+                let page=Math.ceil(props.data.length/props.limit);
+                this.setState({page});
+            } 
+        }
+        
+
+        scroll(e){
+            let div=this.refs.main;
+            if(div.offsetHeight&&!this.state.waiting){//有长度，说明是正在显示
+                let se = document.documentElement.clientHeight; //浏览器可见区域高度。
+                let bottom = div.getBoundingClientRect().bottom-se; //元素底端到可见区域底端的距离
+                if (bottom<50) {
+                    this.setState({waiting:true});
+                    this.props.next();
+                }
+            }
+        }
+        
+        render() {
+            if(this.props.max>=0&&this.props.max<=this.props.data.length){
+                this.componentWillUnmount();
+            }
+            let pages=[];
+            for(let i=0;i<this.state.page;i++)
+                pages.push(<ShowComponent 
+                    data={this.props.data.slice(i*this.props.limit,(i+1)*this.props.limit)}
+                    key={i}
+                />);
+            if(this.state.waiting)
+                pages.push(<ShowComponent 
+                    data={null}
+                    key={pages.length}
+                />);
+            return (
+                <div ref={'main'}>
+                    {pages}
+                </div>
+            );
         }
     }
-    render() {
-        let loading=null;
-        if(this.props.forLoad)
-            loading=(<div ref='loading'>{'正在加载……'}</div>);
-        return (
-            <List {...this.props}>
-                {this.props.children}
-                {loading}
-            </List>
-        );
-    }
 }
-
-AutoList._lists=[];
-
-AutoList.scroll=function(){
-    let sh=window.screen.availHeight;
-    AutoList._lists.forEach(function(element) {
-        let r=element.refs.loading?element.refs.loading.getBoundingClientRect():{};
-        if((r.top||r.bottom)&&r.top<sh)
-            element.load();
-    });
-}
-
 export default AutoList;
